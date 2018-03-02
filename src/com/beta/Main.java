1743aefcb4423fa5785db0326c19ace5f02ed663
Brad Cook <cookbrad@oregonstate.edu>
2018-03-01 16:55:58 -0800

    add core.jar and PlayScreen class

--- src/com/beta/Main.java
+++ src/com/beta/Main.java
@@ -1,8 +1,13 @@
 package com.beta;
 
+import processing.core.PApplet;
+
 public class Main {
 
+    private final static String[] OPTIONS = new String[]{"--present", "PlayScreen"};
+
     public static void main(String[] args) {
+      //  PApplet.main(OPTIONS);
 	// write your code here
     }
 }
---
+++ src/com/beta/PlayScreen.java
@@ -0,0 +1,18 @@
+package com.beta;
+
+import processing.core.PApplet;
+
+public class PlayScreen extends PApplet{
+
+    public void setings() {
+
+    }
+
+    public void setup() {
+        size(500,500);
+    }
+    public void draw() {
+
+    }
+
+}
