b9cb9e38bf2d40f47077d41def583bab79a903d7
Krishan <krishanchopra30@yahoo.com>
2018-03-05 09:54:12 -0800

    Add Krishan circle

--- src/com/beta/PlayScreen.java
+++ src/com/beta/PlayScreen.java
@@ -14,11 +14,17 @@ public class PlayScreen extends PApplet{
     }
     public void draw() {
         drawBradCircle();
+        drawKrishanCircle();
 
     }
     private void drawBradCircle(){
         fill(0,0,255);
         ellipse(height / 2, width / 2, 100, 100);
+    }
+
+    private void drawKrishanCircle(){
+        fill(200, 150, 54);
+        ellipse(a: height / 2, b: width / 2, c: 50, d: 50);
     }
 
 }
