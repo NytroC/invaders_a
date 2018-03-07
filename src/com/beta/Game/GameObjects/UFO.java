047a9e453f5810c2e3803587d6c638c519dffe63
Drew Bartlett <drewb9@gmail.com>
2018-03-07 14:24:43 -0800

    Move sprites to data folder. Update dependent classes

--- src/com/beta/Game/GameObjects/UFO.java
+++ src/com/beta/Game/GameObjects/UFO.java
@@ -14,8 +14,7 @@ public class UFO extends GameObject {
     }
 
     void setup() {
-
-        this.sprite = this.game.loadImage("UFO.png");
+        this.sprite = this.game.loadImage("sprites/ufo.png");
     }
 
     public void draw() {
@@ -28,7 +27,7 @@ public class UFO extends GameObject {
         return this.point;
     }
 
-    private void moveUFO(){
+    private void moveUFO() {
 
         this.point.x += xSpeed;
     }
