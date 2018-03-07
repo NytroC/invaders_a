e84eab59aa0a5700bb71f04136f51292aef72d6b
Brad Cook <cookbrad@oregonstate.edu>
2018-03-06 21:03:27 -0800

    Implament rockets and ship firing rockets. fixes #12, #22.

--- src/com/beta/Game/GameObjects/Rocket.java
+++ src/com/beta/Game/GameObjects/Rocket.java
@@ -1,4 +1,32 @@
 package com.beta.Game.GameObjects;
 
-public class Rocket {
+import com.beta.Game.Screens.PlayScreen;
+
+public class Rocket extends  GameObject {
+    public Rocket(PlayScreen game, Point point) {
+        super(game, point);
+
+        this.setup();
+    }
+
+    void setup() {
+        //        this.shipImage = this.game.loadImage("ship.png");
+    }
+
+    public void draw() {
+//        this.game.image(shipImage, this.point.x, this.point.y);
+        this.game.fill(255, 000, 000);
+        this.game.rect(this.point.x, this.point.y, 10, 10);
+        this.update();
+    }
+    public void update(){
+        this.point.y--;
+    }
+
+    public Point getPosition() {
+        return this.point;
+    }
+
+
+
 }
--- src/com/beta/Game/GameObjects/Ship.java
+++ src/com/beta/Game/GameObjects/Ship.java
@@ -1,8 +1,11 @@
 package com.beta.Game.GameObjects;
 
 import com.beta.Game.Screens.PlayScreen;
+import com.beta.Game.GameObjects.Rocket;
 
 public class Ship extends GameObject {
+
+    private Rocket rocket;
 
     public Ship(PlayScreen game, Point point) {
         super(game, point);
@@ -19,6 +22,12 @@ public class Ship extends GameObject {
         this.game.fill(255, 000, 000);
         this.game.rect(this.point.x, this.point.y, 50, 50);
         this.update();
+        if (this.rocket != null) {
+            this.rocket.draw();
+            if(this.rocket.point.y <= 0){
+                this.rocket = null;
+            }
+        }
     }
 
     public Point getPosition() {
@@ -31,6 +40,13 @@ public class Ship extends GameObject {
                 this.point.x -= 2;
             } else if (game.key == 'd') {
                 this.point.x += 2;
+            }
+        }
+        if (game.keyPressed) {
+            if (game.key == ' ') {
+                float rocketY = this.point.y;
+                float rocketX = this.point.x;
+                this.rocket = new Rocket(this.game, new Point(rocketX, rocketY ));
             }
         }
     }
