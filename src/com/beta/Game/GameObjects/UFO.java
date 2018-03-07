b7e18fab2710d5781311d35137d1c2656fcafe71
Curtis Baney <curtistheiii@gmail.com>
2018-03-07 11:55:16 -0800

    Add UFO Class

--- src/com/beta/Game/GameObjects/UFO.java
+++ src/com/beta/Game/GameObjects/UFO.java
@@ -1,4 +1,38 @@
 package com.beta.Game.GameObjects;
 
-public class UFO {
+import com.beta.Game.Screens.PlayScreen;
+import processing.core.PImage;
+
+public class UFO extends GameObject {
+    private PImage sprite;
+    private float xSpeed = 1.0f;
+
+    public UFO(PlayScreen game, Point point) {
+
+        super(game, point);
+        this.setup();
+    }
+
+    void setup() {
+
+        this.sprite = this.game.loadImage("UFO.png");
+    }
+
+    public void draw() {
+//        this.game.image(shipImage, this.point.x, this.point.y);
+        moveUFO();
+        this.game.image(sprite, this.point.x, this.point.y);
+    }
+
+    public Point getPosition() {
+        return this.point;
+    }
+
+    private void moveUFO(){
+
+        this.point.x += xSpeed;
+    }
+
+
+
 }
--- src/com/beta/Game/Screens/PlayScreen.java
+++ src/com/beta/Game/Screens/PlayScreen.java
@@ -4,12 +4,14 @@ import com.beta.Game.Contracts.GameScreen.GameScreenInterface;
 import com.beta.Game.GameObjects.AlienFleet;
 import com.beta.Game.GameObjects.Point;
 import com.beta.Game.GameObjects.Ship;
+import com.beta.Game.GameObjects.UFO;
 import processing.core.PApplet;
 
 public class PlayScreen extends PApplet implements GameScreenInterface {
 
     protected AlienFleet alienFleet;
     protected Ship ship;
+    protected UFO ufo;
 
     // this can probably move to a main screenController
     public void settings() {
@@ -23,11 +25,13 @@ public class PlayScreen extends PApplet implements GameScreenInterface {
 
         this.alienFleet = new AlienFleet(this, new Point(50, 50));
         this.ship = new Ship(this, new Point(50, this.height - 50));
+        this.ufo = new UFO(this, new Point(-300,25));
     }
 
     public void draw() {
         alienFleet.draw();
         ship.draw();
+        ufo.draw();
     }
     public void keyPressed(){
         if(key == ' '){
