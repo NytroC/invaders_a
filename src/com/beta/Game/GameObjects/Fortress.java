0b1d33a397ff0fc1bfd942b732a2c705eb7b1100
Krishan <krishanchopra30@yahoo.com>
2018-03-06 21:52:20 -0800

    Create Fortress positioning and life remaining variables

--- src/com/beta/Game/GameObjects/Fortress.java
+++ src/com/beta/Game/GameObjects/Fortress.java
@@ -1,4 +1,28 @@
 package com.beta.Game.GameObjects;
 
+import processing.core.PApplet;
+
 public class Fortress {
+
+    private int x;
+    private int y;
+    private int lifeRemaining;
+    private PApplet p;
+
+    public Fortress(int x, int y, int lifeRemaining, PApplet p) {
+        this.x = x;
+        this.y = y;
+        this.lifeRemaining = lifeRemaining;
+        this.p = p;
+    }
+
+    private void fortressDecay(int status) {
+        lifeRemaining -= status;
+    }
+
+    public void draw() {
+        p.fill(125, 0, 0);
+        p.ellipse(x, y, 2, 3);
+    }
 }
+
