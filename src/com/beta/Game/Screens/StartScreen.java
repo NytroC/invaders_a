e9649044aa937ca145d42812b71b2af0482e70c1
Brad Cook <cookbrad@oregonstate.edu>
2018-03-13 13:44:04 -0700

    Add start screen text

--- src/com/beta/Game/Screens/StartScreen.java
+++ src/com/beta/Game/Screens/StartScreen.java
@@ -5,20 +5,26 @@ import processing.core.PImage;
 
 public class StartScreen {
 
-    protected PImage startImage;
     protected Game game;
     protected String highScoreText;
 
     public StartScreen(Game game){
         this.game = game;
         highScoreText = "High Score: ";
-        this.startImage = game.loadImage("sprites/start.png");
-
     }
 
     public void draw(){
+        game.fill(200, 255, 2);
+        game.textSize(75);
+        game.text(game.gameState.title, 150, game.height / 2);
+        game.textSize(32);
         game.text(highScoreText + game.gameState.getHighScore(), 300, 50);
-        game.image(startImage, 300, 300);
+        game.text("Insert Coin(s)", 300, game.height / 2 + 50);
+        game.text("Credit(s): 3", 325, 600);
+        if (game.frameCount % 60 < 20){
+            game.fill(0, 0, 0);
+        }
+        game.text("Any Button To Continue", 200, 400);
     }
 
 }
